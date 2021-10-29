4fd67fead3c8551cc9f75d0c2d88e3dfe4bb18b8
17eugene <drivercoua@gmail.com>
2021-10-29 23:59:37 +0300

    Upgrade

--- contacts.js
+++ contacts.js
@@ -3,14 +3,6 @@ const getContactById = require("./contactsOperations/getContactById");
 const addContact = require("./contactsOperations/addContact");
 const deleteContactById = require("./contactsOperations/deleteContact");
 
-// function removeContact(contactId) {
-//   // ...твой код
-// }
-
-// function addContact(name, email, phone) {
-//   // ...твой код
-// }
-
 module.exports = {
   getListOfContacts,
   getContactById,
